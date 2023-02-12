58ebc1eae2186bb3d2ae1efb3ba45ba4d077dc12
Anumod <anumod.ot@gmail.com>
2023-02-12 00:37:12 -0500

    Initial commit

---
+++ src/OpenCL.h
@@ -0,0 +1,9 @@
+﻿// Author: Anumod
+// OpenCL.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
